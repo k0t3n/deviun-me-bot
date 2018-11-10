6b39f3b0b0f18fc382cb7bf9a0becfd3b151a270
Anton Danilov <a.deviun@gmail.com>
2018-11-10 15:53:30 +0300

    delete useless variables

--- src/handler/start.js
+++ src/handler/start.js
@@ -1,9 +1,5 @@
-const config = require('config');
 const Lang = require('../lib/lang');
-
 const lang = new Lang();
-
-const ADMIN = config.get('bot.adminId');
 
 module.exports = (ctx) => {
   const isAdmin = ctx.from.is_admin;
