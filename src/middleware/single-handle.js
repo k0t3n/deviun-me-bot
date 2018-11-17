3aa2eee4f050f24702540702f661546225521fc5
Anton Danilov <a.deviun@gmail.com>
2018-11-17 09:55:57 +0300

    use get sender util

--- src/middleware/single-handle.js
+++ src/middleware/single-handle.js
@@ -1,7 +1,9 @@
 const af = require('../lib/action-flow');
+const get = require('lodash/get');
+const getSender = require('../util/get-sender');
 
 module.exports = async (ctx, next) => {
-  const { id } = ctx.from;
+  const { id } = getSender(ctx);
   const flow = af.create({
     description: 'user single operation',
     userId: id
--- src/middleware/user-ctx.js
+++ src/middleware/user-ctx.js
@@ -1,7 +1,9 @@
 const UserCtx = require('../module/user-context');
+const getSender = require('../util/get-sender');
 
 module.exports = async (ctx, next) => {
-  ctx.userCtx = await new UserCtx(ctx.from.id).load();
+  const { id } = getSender(ctx);
+  ctx.userCtx = await new UserCtx(id).load();
   await next();
   ctx.userCtx.save();
 };
